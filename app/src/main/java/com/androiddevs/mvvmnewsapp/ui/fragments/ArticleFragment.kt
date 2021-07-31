a730a3e27a35dab45638ba7bc311c72a74b19b43
abhishek arora <abhishekarora873@gmail.com>
2021-07-31 19:27:51 +0530

    News-App

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/fragments/ArticleFragment.kt
@@ -0,0 +1,61 @@
+package com.androiddevs.mvvmnewsapp.ui.fragments
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
+import android.webkit.WebViewClient
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
+import com.androiddevs.mvvmnewsapp.R
+import com.androiddevs.mvvmnewsapp.ui.NewsActivity
+import com.androiddevs.mvvmnewsapp.ui.NewsViewModel
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.fragment_article.*
+
+class ArticleFragment: Fragment(R.layout.fragment_article) {
+
+    lateinit var viewModel: NewsViewModel
+    val args: ArticleFragmentArgs by navArgs()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = (activity as NewsActivity).viewModel
+        val article = args.article
+
+        setHasOptionsMenu(true)
+
+        webView.apply {
+            webViewClient = WebViewClient()
+            loadUrl(article.url)
+        }
+
+        fab.setOnClickListener {
+            viewModel.saveArticle(article)
+            Snackbar.make(view, "Article saved successfully", Snackbar.LENGTH_SHORT).show()
+        }
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.share_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.share -> startActivity(getIntent())
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun getIntent() : Intent {
+        val intent = Intent(Intent.ACTION_SEND)
+        intent.type = "text/plain"
+        intent.putExtra(Intent.EXTRA_TEXT, args.article.url)
+        return intent
+    }
+
+}
---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,10 @@
+package com.androiddevs.mvvmnewsapp.util
+
+class Constants {
+    companion object {
+        const val API_KEY = "8874286548504f04bf96ce3163d5ec67"
+        const val BASE_URL = "https://newsapi.org"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
+        const val QUERY_PAGE_SIZE = 20
+    }
+}
